ec119b8c90bf098aad2692be8b43ffd8ac6fa2df
AppResuelve <appresuelveok@gmail.com>
2026-06-03 09:45:02 -0300

    fix: remove zod schemas from shared barrel to fix Vite build

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -1,3 +1,2 @@
 export { FORM_SECTIONS, CLIENT_STATUS, DOCUMENT_TYPES } from './constants.js';
 export { apiFetch, getClientByToken, getOnboardingData, saveOnboardingData, uploadDocument, getDocuments, deleteDocument } from './api.js';
-export { UuidSchema, EmailSchema, TokenSchema, UrlSchema, CreateClientSchema, UpdateClientSchema, SaveOnboardingSchema, DocumentTypeSchema, UploadDocumentSchema } from './schemas/index.js';
